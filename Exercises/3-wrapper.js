8df9b36c54a44df05741a4da0e7057c9ab939b05
DimaBohdan <bohdan777.dmytro@gmail.com>
2024-10-31 20:34:17 +0200

    labs completed

--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,24 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+const contract = (fn, ...types) => (...args) => {
+  types.slice(0, -1).forEach((type, i) => {
+    if (typeof args[i] !== type.name.toLowerCase()) {
+        throw new TypeError(`Argument type ${type.name.toLowerCase()} expected`);
+      }
+    });
+  
+    const result = fn(...args);
+    const resultType = types[types.length - 1];
+    if (typeof result !== resultType.name.toLowerCase()) {
+      throw new TypeError(`Result type ${resultType.name.toLowerCase()} expected`);
+    }
+  
+    return result;
+  };
+
+const concat = (s1, s2) => s1 + s2;
+const concatStrings = contract(concat, String, String, String);
+const res = concatStrings('Hello ', 'world!');
+console.dir(res); // Output: Hello world!
 
 module.exports = { contract };
